d8a5746b539370a3d216e6d63a0e5e458c191c97
bbane <ssj7410@gmail.com>
2021-06-21 23:34:08 +0900

    Initial commit

---
+++ src/main/java/com/study/batismapping/BatisMappingApplication.java
@@ -0,0 +1,13 @@
+package com.study.batismapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatisMappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatisMappingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/batismapping/BatisMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.batismapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatisMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
