1f1285b371816cc70ca7b7dca0181635a7d6f0a7
bbane <ssj7410@gmail.com>
2021-06-21 23:46:04 +0900

    debug

--- src/main/java/com/study/batismapping/CompanyMapper.java
+++ src/main/java/com/study/batismapping/CompanyMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface CompanyMapper {
 
-    @Insert("insert into company(company_name, company_address) values(#{company.name}, #{company.address}")
+    @Insert("insert into company(company_name, company_address) values(#{company.name}, #{company.address})")
     int insert(@Param("company") Company company);
 }
 
